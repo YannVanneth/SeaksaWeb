a9341de7831d7284e5f66fdcf98e9044b71fe334
YannVanneth <yannvanneth.27@gmail.com>
2025-07-08 00:11:48 +0700

    [FIX] vite add outDir

--- Front-End/vite.config.js
+++ Front-End/vite.config.js
@@ -4,10 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  outDir: 'public',
   server: {
     port: 3000, // Change Vite development server port to 3000
-    proxy: {
-      "/auth/register": "http://localhost:3000", // Proxy API requests to backend
-    },
   },
 });
